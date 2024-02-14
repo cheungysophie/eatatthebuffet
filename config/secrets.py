2b37582ab1c6a93c7a82177bff89672f5c9875de
sophie <cheungysophie@gmail.com>
2024-02-13 18:47:53 -0800

    remove secrets

--- config/secrets.py
+++
@@ -1,8 +0,0 @@
-admin_email = 'cheungysophie@gmail.com'
-admin_password = 'rukkoxlryfjarcqv'
-server = 'smtp.gmail.com'
-port = 587
-ALPHA_API = 'CGPUTRPQN77QCW4E'
-newsKey = '6615768baeb74ed9850f5cff26953d38'
-OPEN_API = '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'
-OPEN_ORG = 'org-m8lzkNej3ZzWn8hYmFVlWYv9'
